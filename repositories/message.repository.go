01319fdb60a7acf284615cfaa888c539ef2ad35f
AyrtonBLD <ayrton.blondeau@ynov.com>
2026-06-12 10:57:57 +0200

    feat: filtre Repository et URL router

--- repositories/message.repository.go
+++ repositories/message.repository.go
@@ -21,3 +21,24 @@ func (r *MessageRepository) Create(msg *models.Message) error {
 	_, err := r.db.Exec(query, msg.ID, msg.Contenu, msg.DateEnvoi, msg.ThreadID, msg.UserID, msg.Score)
 	return err
 }
+// GetByThreadID y récupère tous les messages associés à une annonce spécifique
+func (r *MessageRepository) GetByThreadID(threadID string) ([]models.Message, error) {
+	query := `SELECT id_message, contenu, date_envoi, id_fil, id_user, score 
+			  FROM message WHERE id_fil = ? ORDER BY date_envoi ASC`
+	
+	rows, err := r.db.Query(query, threadID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var messages []models.Message
+	for rows.Next() {
+		var m models.Message
+		if err := rows.Scan(&m.ID, &m.Contenu, &m.DateEnvoi, &m.ThreadID, &m.UserID, &m.Score); err != nil {
+			return nil, err
+		}
+		messages = append(messages, m)
+	}
+	return messages, nil
+}
