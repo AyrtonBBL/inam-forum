6115a3df000cae76574d7eecebf81d018bda3eed
AyrtonBLD <ayrton.blondeau@ynov.com>
2026-06-12 10:53:07 +0200

    feat: fichier Repository et routes pour les msg modif bug

--- repositories/message.repository.go
+++ repositories/message.repository.go
@@ -17,7 +17,7 @@ func InitMessageRepository(db *sql.DB) *MessageRepository {
 func (r *MessageRepository) Create(msg *models.Message) error {
 	query := `INSERT INTO message (id_message, contenu, date_envoi, id_fil, id_user, score) 
 			  VALUES (?, ?, ?, ?, ?, ?)`
-	
-	_, err := r.db.Exec(query, msg.ID, msg.Contenu, msg.DateEnvoi, msg.IDFil, msg.UserID, msg.Score)
+
+	_, err := r.db.Exec(query, msg.ID, msg.Contenu, msg.DateEnvoi, msg.ThreadID, msg.UserID, msg.Score)
 	return err
 }
